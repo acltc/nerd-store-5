c482e411e58d26e5b03818ac3182762507d17f17
JackMarx <seriousfools@gmail.com>
2016-09-07 14:05:29 -0500

    rails 5 version

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,50 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+  end
+
+  def create
+    @product = Product.create(
+      name: params[:name],
+      description: params[:description],
+      image: params[:image],
+      price: params[:price]
+      )
+
+    flash[:success] = "Product Created"
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params[:name],
+      description: params[:description],
+      image: params[:image],
+      price: params[:price]
+      )
+
+    flash[:success] = "Product Updated"
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+
+    flash[:warning] = "Product Created"
+    redirect_to "/"
+  end
+end
+
