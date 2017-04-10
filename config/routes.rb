d5f3f82b41679cbfc787f002a2f69f64f88885c4
Peter Jang <peter.x.jang@gmail.com>
2017-04-10 17:50:40 +0000

    Fix delete route typo

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get '/products/:id' => 'products#show'
   get '/products/:id/edit' => 'products#edit'
   patch '/products/:id' => 'products#update'
-  delete '/products/:id' => 'products#delete'
+  delete '/products/:id' => 'products#destroy'
 end
