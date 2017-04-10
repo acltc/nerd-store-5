5b259e77da7d9b3473b460dcee65070b45138fe0
Peter Jang <peter.x.jang@gmail.com>
2017-04-10 18:02:46 +0000

    Update code style

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    render "index.html.erb"
   end
 
   def new
+    render "new.html.erb"
   end
 
   def create
@@ -12,18 +14,19 @@ class ProductsController < ApplicationController
       description: params[:description],
       image: params[:image],
       price: params[:price]
-      )
-
+    )
     flash[:success] = "Product Created"
     redirect_to "/products/#{@product.id}"
   end
 
   def show
     @product = Product.find_by(id: params[:id])
+    render "show.html.erb"
   end
 
   def edit
     @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
   end
 
   def update
@@ -33,8 +36,7 @@ class ProductsController < ApplicationController
       description: params[:description],
       image: params[:image],
       price: params[:price]
-      )
-
+    )
     flash[:success] = "Product Updated"
     redirect_to "/products/#{@product.id}"
   end
@@ -42,8 +44,7 @@ class ProductsController < ApplicationController
   def destroy
     @product = Product.find_by(id: params[:id])
     @product.destroy
-
-    flash[:warning] = "Product Created"
+    flash[:warning] = "Product Destroyed"
     redirect_to "/"
   end
 end
