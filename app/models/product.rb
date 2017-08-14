99daeb95a5541d36e2ec00374d3d784cc59ee5e1
Brian <riceb53@gmail.com>
2017-08-14 18:27:50 -0400

    add style

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,36 @@
 class Product < ActiveRecord::Base
+  def sale_message
+    # return a message(string) based on the price
+    if price.to_f <= 2
+      return "Discount Item!"
+    else
+      return "Everyday Value!"
+    end
+  end
+
+  def tax
+    return price.to_f * 0.09
+  end
+
+  def total
+    return price.to_f + tax
+  end
+
+  def cheap?
+    if sale_message == "Discount Item!"
+      return true
+    else
+      return false
+    end
+  end
+
+  def sale_item_class
+    if sale_message == "Discount Item!"
+      return "discount-item"
+    else
+      return ""
+    end
+  end
 end
+
+
